1520e654cda82780c38b26656e2d8891721807da
egorsheremet <81770845+egorsheremet@users.noreply.github.com>
2026-05-16 01:50:31 +0300

    Update LevelLifetimeScope.cs

--- Assets/Level/Runtime/Scopes/LevelLifetimeScope.cs
+++ Assets/Level/Runtime/Scopes/LevelLifetimeScope.cs
@@ -27,8 +27,8 @@ namespace Level.Runtime.Scopes
         [Expandable]
         [SerializeField] private GearDataRpm gearDataRpm;
 
-        [SerializeField] private SoulData        soulData;
-        [SerializeField] private HealthData      healthData;
+        [SerializeField] private SoulData soulData;
+        [SerializeField] private HealthData healthData;
 
         protected override void Configure(IContainerBuilder builder)
         {
@@ -38,33 +38,31 @@ namespace Level.Runtime.Scopes
                 return;
             }
 
+            // Physics & Car Data
             builder.RegisterInstance(physicsData);
             builder.RegisterInstance(nitroData);
             builder.RegisterInstance(gearDataRpm);
+            builder.RegisterInstance(new RoadCheckService(surfaceMask));
 
+            // Core Services & States
             builder.Register<TransmissionService>(Lifetime.Singleton);
+            builder.Register<CarService>(Lifetime.Singleton);
+            builder.Register<CarPhysicsService>(Lifetime.Singleton);
+            builder.Register<NitroService>(Lifetime.Singleton);
 
             builder.Register<InputService>(Lifetime.Singleton)
                 .AsSelf()
                 .As<IInitializable>()
                 .As<IDisposable>();
+
             builder.Register<DriveCarState>(Lifetime.Singleton)
                 .AsSelf()
                 .As<BaseCarState>();
             builder.Register<DriftCarState>(Lifetime.Singleton)
                 .AsSelf()
                 .As<BaseCarState>();
-            builder.Register<CarService>(Lifetime.Singleton);
-            builder.Register<CarPhysicsService>(Lifetime.Singleton);
-            builder.Register<NitroService>(Lifetime.Singleton);
-            builder.RegisterInstance(new RoadCheckService(surfaceMask));
-            builder.RegisterComponentInHierarchy<CarController>();
-            builder.RegisterComponentInHierarchy<DynamicCameraController>();
 
-            builder.RegisterComponentInHierarchy<GearDisplayUI>();
-            builder.RegisterComponentInHierarchy<TachometerController>();
-<<<<<<< HEAD
-
+            // Soul & Health Systems
             builder.RegisterInstance(soulData);
             builder.RegisterInstance(healthData);
 
@@ -72,11 +70,22 @@ namespace Level.Runtime.Scopes
             builder.Register<SoulDrainService>(Lifetime.Singleton).AsImplementedInterfaces().AsSelf();
             builder.Register<HealthService>(Lifetime.Singleton).AsImplementedInterfaces().AsSelf();
 
+            // Combat & Target Systems
+            builder.Register<TargetRegistry>(Lifetime.Singleton);
+
+            // In-Hierarchy Components (MonoBehaviours)
+            builder.RegisterComponentInHierarchy<CarController>();
+            builder.RegisterComponentInHierarchy<DynamicCameraController>();
+            builder.RegisterComponentInHierarchy<GearDisplayUI>();
+            builder.RegisterComponentInHierarchy<TachometerController>();   
             builder.RegisterComponentInHierarchy<DebugSoulHealthTester>();
             builder.RegisterComponentInHierarchy<DualBarController>();
             builder.RegisterComponentInHierarchy<EnemyHealth>();
-            builder.Register<TargetRegistry>(Lifetime.Singleton);
-            
+
+            /*builder.RegisterBuildCallback(resolver =>
+            {
+                resolver.InjectGameObject(gameObject);
+            });*/
         }
     }
 }
